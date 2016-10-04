03c5a00d6ff06ff4f02f2963749d73e073939afd
Takuya Murakami <murakami@da.jp.nec.com>
2016-10-04 20:38:44 +0900

    fix: context.logger.log is not exists.

--- sample1/index.js
+++ sample1/index.js
@@ -10,7 +10,7 @@ exports.sayHello = function (event, context) {
         message: "Hello world, " + name + " !!!"
     };
 
-    context.logger.log("response = " + JSON.stringify(response));
+    context.logger.debug("response = " + JSON.stringify(response));
     context.succeed(response);
 };
 
